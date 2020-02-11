a0e18cfbbb39bf9c7ff549e4ed13aaa0e24199ee
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-11 01:21:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
-
 require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -9,7 +9,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def crew
+def crew_array
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
@@ -21,5 +21,5 @@ end
 def engage
   date = generate_star_date
   state_log(date)
-  greet_crew(crew)
+  greet_crew(crew_array)
 end
